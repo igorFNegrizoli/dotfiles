5b0c0ff02de12ec63f8c91ed8853385d69ac66a2
Igor F. Negrizoli <igor.negrizoli@gmail.com>
2026-02-24 19:18:20 -0300

    Adjustments to run the container

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 # init submodules (only ralph at the time of this script creation)
 git submodule update --init --recursive
 cp -r deps/ralph/skills/. claude-runner/skills/
+# create the empty file so I can save progress if a session ends but still dont have it on the dotfile repo
+touch claude-runner/todo.txt
 
