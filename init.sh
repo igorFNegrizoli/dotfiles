a32cea01afa61d6eddb493c905fd2044f8263e64
Igor F. Negrizoli <igor.negrizoli@gmail.com>
2026-02-25 15:20:05 -0300

    Add agents and adjust container mount

--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@ git submodule update --init --recursive
 cp -r deps/ralph/skills/. claude-runner/skills/
 # create the empty file so I can save progress if a session ends but still dont have it on the dotfile repo
 touch claude-runner/todo.txt
-cp -R claude/skills/ ~/.claude/
-cp claude/CLAUDE.md ~/.claude/
 #symlink the config files to the actual path claude uses (clade code does not uses XDG Base Directory Specification)
-ln -sf ~/.config/claude/CLAUDE.md ~/.claude/CLAUDE.md
-ln -sf ~/.config/claude/skills ~/.claude/
+mkdir -p ~/.claude/
+ln -sfn ~/.config/claude/CLAUDE.md ~/.claude/CLAUDE.md
+ln -sfn ~/.config/claude/skills ~/.claude/
+ln -sfn ~/.config/claude/agents ~/.claude/
 echo 'Dont forget to setup your MCP servers'
 
 
