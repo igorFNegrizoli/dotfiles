382c8ddf000b7d348487cbf6145eda931dfbb08e
Igor F. Negrizoli <igor.negrizoli@gmail.com>
2026-02-24 22:51:44 -0300

    Sync skills and .md and setup symlink

---
+++ claude/skills/daily_report/SKILL.md
@@ -0,0 +1,3 @@
+# Daily Report
+
+Write me a daily report from what you find on the context of this conversation..
--- init.sh
+++ init.sh
@@ -3,4 +3,9 @@ git submodule update --init --recursive
 cp -r deps/ralph/skills/. claude-runner/skills/
 # create the empty file so I can save progress if a session ends but still dont have it on the dotfile repo
 touch claude-runner/todo.txt
+cp -R claude/skills/ ~/.claude/
+cp claude/CLAUDE.md ~/.claude/
+ln -sf ~/.config/claude/CLAUDE.md ~/.claude/CLAUDE.md
+ln -sf ~/.config/claude/skills ~/.claude/
+
 
