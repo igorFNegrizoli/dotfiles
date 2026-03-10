183419ee67032ea7be33d5c81a9195a20ffc2ed7
Igor F. Negrizoli <igor.negrizoli@gmail.com>
2026-03-10 15:18:41 -0300

    ZSH adjustments and brave incognito policy

--- fedora_atomic_setup.sh
+++ fedora_atomic_setup.sh
@@ -1,5 +1,6 @@
 echo "Running the setup for Fedora Atomic 43 (Cosmic)"
 sudo -v
+
 echo "Adding Flatpaks"
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.brave.Browser org.qbittorrent.qBittorrent com.spotify.Client org.dbgate.DbGate org.videolan.VLC md.obsidian.Obsidian -y
@@ -10,12 +11,10 @@ echo "Applying brave policies"
 sudo mkdir -p /etc/brave/policies/managed/
 sudo ln -sfn ~/.config/brave/policy.json /etc/brave/policies/managed/
 sudo flatpak override com.brave.Browser --filesystem="/etc/brave/policies/managed:ro" --filesystem="~/.config/brave:ro"
-echo "Creating toolbox and installing htop, fastfetch"
-toolbox create -y
-toolbox run sudo dnf install -y htop fastfetch
-# no tmux and alacritty for now
-echo "installing layered packages via rpm-ostree"
+
+echo "Installing layered packages via rpm-ostree"
 rpm-ostree install -y zsh moby-engine docker-compose
+
 echo "Installing Oh My Zsh..."
 ZSH="$HOME/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 echo "Write zsh config"
@@ -23,8 +22,15 @@ sudo usermod -s $(which zsh) $USER
 mkdir -p ~/.local/bin
 mkdir -p ~/.config/zsh
 ln -sf "$HOME/.config/zsh/.zshenv" "$HOME/.zshenv"
+
 echo "Allowing custom scripts"
 chmod +x ~/.config/scripts/*
+
+echo "Creating toolbox and installing htop, fastfetch"
+toolbox create -y
+toolbox run sudo dnf install -y htop fastfetch zsh
+toolbox run sudo chsh -s /usr/bin/zsh $USER
+# no tmux and alacritty for now
 
 echo "Starting Claude Code setup - No claude-runner container being set (will have its own repo)"
 # init submodules (only ralph at the time of this script creation)
